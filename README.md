10180050899cff8054d7619fba8c2bdf57f15163
Stehlik Lukas <stehlik.lukas@gmail.com>
2017-05-25 09:40:33 +0200

    Script added to repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # devstack-OC-prerequisites
 Script for necessary prerequisites for successful deployment of Devstack with OpenContrail
+
+I was running devstack over and over again in different VMs... So i have put together simple script to install all requirements to run devstack successfuly.
---
+++ devstackOC-prereq.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+echo "Devstack + OpenContrail prerequisites installation script"
+
+function deploy {
+	printf "Do you want to do sudo apt-get update [N/y]: "
+	read -r updt
+	if [ $updt == y ] || [ $updt == Y ];
+		then
+		sudo apt-get update
+	fi
+
+	sudo apt-get install -y python-pip
+	sudo pip install --upgrade pip
+	sudo pip install -U os-testr
+	sudo apt-get install -y git
+	git config --global url."https://".insteadOf git://
+	git clone https://github.com/openstack-dev/devstack.git
+	cd devstack
+
+
+	printf "What branch (Devstack release) do you want to use ['stable/mitaka']: "
+	read -r branch
+	if [ -z $branch ] ;
+		then
+		branch="stable/mitaka"
+	fi
+	git checkout $branch
+
+
+	printf "What branch (OpenContrail release) do you want to use [R3.1.1.x]: "
+	read -r OC
+	if [ -z $OC ] ;
+		then
+		OC="R3.1.1.x"
+	fi
+
+	cp samples/local.conf .
+	echo "enable_plugin contrail https://github.com/zioc/contrail-devstack-plugin.git" >> local.conf
+	OC="CONTRAIL_BRANCH=$OC" 
+	echo $OC >> local.conf
+
+	printf "Ready to run Devstack+OC [n/Y]? " 
+	read -n 1 -r run
+	if [ -z $run ] || [ $run == y ] || [ $run == Y ];
+		then
+		./stack.sh
+	fi
+}
+
+printf "What do you want to do [1]: \n\r 1 - install prerequisites, clone Devstack+OC repo and run devstack \n\r 2 - unstack \n\r 3 - unstack and clean \n\r"
+read slct
+case "$slct" in
+	1)
+		deploy
+	;;
+	2)
+		cd devstack
+		./unstack.sh
+	;;
+	3)
+		cd devstack
+		./unstack.sh
+		./clean.sh		
+	;;
+	*)
+		deploy
+	;;
+esac
+
+#	error s ssl / kafka build failne kvuli ssl
+#	sudo apt-get install libssl-dev libsasl2-dev liblz4-dev
+#	upravit: controller/src/analytics/SConscript -- pridat "'-lssl', '-lcrypto', '-lpthread',"
+#	if sys.platform != 'darwin':
+#    AnalyticsEnv.Prepend(LINKFLAGS =
+#                         ['-Wl,--whole-archive',
+#                          '-lbase', '-lcpuinfo',
+#                          '-lprocess_info', '-lnodeinfo',
+#                          '-l:librdkafka.a', '-l:librdkafka++.a','-lssl', '-lcrypto', '-lpthread',
+#                          '-Wl,--no-whole-archive'])
